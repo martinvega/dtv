7e7aaac0d12f6c54fd16c595945bcec2676fff53
Martín <martin.vega7@gmail.com>
2012-03-14 18:19:48 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dtv::Application.config.secret_token = '7d29e3b76168d23de80fc1b1d39e21dd80e1aa50e0a004a43c1a12093d62d6de3011af77656fb6dd6e2e5770f3b9334678e489ed64076d6e14f85d13c1cb044c'
