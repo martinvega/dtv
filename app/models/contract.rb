8a8c7c765d83a1ebd56dcab900b91d2f408f15d2
Martín <martin.vega7@gmail.com>
2012-03-22 16:31:11 -0300

    Mensajes de error en validaciones

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,11 +2,12 @@ class Contract
   include Validatable
   attr_accessor :nombre, :dni, :email, :telefono, :interesado_en, :comentarios
   
-  validates_presence_of :nombre, :dni, :telefono, :email
-  validates_numericality_of :dni, :only_integer => true
-  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-  validates_length_of :nombre, :email, :telefono, :maximum => 30
-  validates_length_of :comentarios, :maximum => 255
+  validates_presence_of :nombre, :dni, :telefono, :email, :message => 'no puede estar en blanco'
+  validates_numericality_of :dni, :only_integer => true, :message => 'debe ser un número'
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, 
+    :message => 'el formato no es válido'
+  validates_length_of :nombre, :email, :telefono, :maximum => 30, :message => 'es demasiado largo'
+  validates_length_of :comentarios, :maximum => 255, :message => 'es demasiado largo'
   
   def initialize(attributes = {})
     self.nombre = attributes[:nombre]
