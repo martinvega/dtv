3cc27a8011e1f41c9f8eabbe6c304ad4594c93e4
Martín <martin.vega7@gmail.com>
2012-03-28 17:28:47 -0300

    Cambios mailer consultas

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -4,10 +4,10 @@ Dtv::Application.config.action_mailer.default_url_options = {
 Dtv::Application.config.action_mailer.raise_delivery_errors = true
 Dtv::Application.config.action_mailer.delivery_method = :smtp
 Dtv::Application.config.action_mailer.smtp_settings = {
-  :address => 'smtp.gmail.com',
-  :domain => 'multisatdigital.com.ar',
+  :address => 'multisat.com.ar',
+  :domain => 'multisat.com.ar',
   :port => 25,
-  :user_name => 'consulta_web@multisatdigital.com.ar',
+  :user_name => 'consulta_web@multisat.com.ar',
   :password => '12345',
   :authentication => :plain
 }
