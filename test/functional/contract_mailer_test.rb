f81feceb71bdd09e5749167f0a5dfc12b30fa819
Martín Vega <martin.vega7@gmail.com>
2012-05-16 12:51:36 -0300

    Pruebas funcionales en mailer

--- test/functional/contract_mailer_test.rb
+++ test/functional/contract_mailer_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ContractMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Send consultation' do
+    contract = Contract.new(:nombre => :Pepito, :dni => 30123456, 
+      :telefono => 4440055, :interesado_en => DIRECTV_OPTIONS.first, 
+      :email => 'pepito@gmail.com', :comentarios => 'Comentarios...')
+    mail = ContractMailer.send_consultation(contract)
+    assert_equal 'Consulta del sitio multisat', mail.subject
+    assert_equal [contract.email], mail.to
+    assert_equal ['soporte@multisat.com.ar'], mail.from
+    assert_match "#{contract.nombre}", mail.body.encoded
+ 
+    assert_difference 'ActionMailer::Base.deliveries.size' do
+      mail.deliver
+    end
+  end
 end
