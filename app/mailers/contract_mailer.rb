7b309203058566a40556e75a35f75e4af45b5c07
Martín Vega <martin.vega7@gmail.com>
2012-05-16 12:50:34 -0300

    Modificaciones en mailer

--- app/mailers/contract_mailer.rb
+++ app/mailers/contract_mailer.rb
@@ -1,11 +1,11 @@
 class ContractMailer < ActionMailer::Base
-  default :from => "consultas_web@multisat.com.ar"
+  default :from => 'soporte@multisat.com.ar'
   
   def send_consultation(contract)
     @contract = contract
     mail(
       :to => contract.email, 
-      :subject => 'Consulta de la web'
+      :subject => 'Consulta del sitio multisat'
     )
   end
 end
