9ab34eeee93f59f8967722fbc73052ccedafd771
Martín Vega <martin.vega7@gmail.com>
2012-07-16 14:05:22 -0300

    Actualización esquema de base de datos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120322215345) do
+ActiveRecord::Schema.define(:version => 20120606005927) do
 
   create_table "campaigns", :force => true do |t|
     t.datetime "created_at"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(:version => 20120322215345) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "comment"
+    t.date     "modification_date"
+    t.string   "payment_form"
   end
 
   create_table "users", :force => true do |t|
@@ -44,6 +46,7 @@ ActiveRecord::Schema.define(:version => 20120322215345) do
     t.datetime "updated_at"
     t.boolean  "admin"
     t.string   "salt"
+    t.boolean  "category2"
   end
 
 end
